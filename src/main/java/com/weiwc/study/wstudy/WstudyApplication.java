c48ccdb6d92e37fe2f1d0af0203ecbf1d993d785
威宸 <weiwc816@gmail.com>
2020-04-26 11:48:02 +0800

    项目初始化，以spring boot搭建初始化项目

---
+++ src/main/java/com/weiwc/study/wstudy/WstudyApplication.java
@@ -0,0 +1,13 @@
+package com.weiwc.study.wstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiwc/study/wstudy/WstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiwc.study.wstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
